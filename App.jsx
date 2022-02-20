f712546dca22c401cea9f01c92c0363611dd1ad4
pitoge <62818292+pitoge@users.noreply.github.com>
2022-02-20 16:02:18 +0900

    Typo Modifying

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ import { StyleSheet, View } from 'react-native';
 
 import AppBar from './src/components/AppBar';
 import MemoList from './src/components/MemoList';
-import CircleButton from './src/components/circleButton';
+import CircleButton from './src/components/CircleButton';
 
 export default function App() {
   return (
