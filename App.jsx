d47ad81b13222d2c44ad6e5578b5f2948af20b5d
pitoge <62818292+pitoge@users.noreply.github.com>
2022-02-20 17:12:51 +0900

    Create MemoDetailScreen

--- App.jsx
+++ App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
-  return <MemoListScreen />;
+  return <MemoDetailScreen />;
 }
