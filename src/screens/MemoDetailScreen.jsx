9ed01da4d8855d26062d0b66b8843a1403b46181
pitoge <62818292+pitoge@users.noreply.github.com>
2022-02-26 18:29:34 +0900

    Update memo data on MemoEditScreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -51,7 +51,10 @@ export default function MemoDetailScreen(props) {
         style={{ top: 60, bottom: 'auto' }}
         name="pencil"
         onPress={() => {
-          navigation.navigate('MemoEdit');
+          navigation.navigate('MemoEdit', {
+            id: memo.id,
+            bodyText: memo.bodyText,
+          });
         }}
       />
     </View>
